7df8c2dc040f85300411eb0e181c28ff957c12b8
nauman3s <naumanshakir3s@gmail.com>
2021-04-19 00:40:45 +0500

    added user reg/login with backend and integ with frontend

--- FrontEnd/BackupComputer/src/pages/Layout/TopNavbar.vue
+++ FrontEnd/BackupComputer/src/pages/Layout/TopNavbar.vue
@@ -16,15 +16,16 @@
         </md-button>
 
         <div class="md-collapse">
-          <div class="md-autocomplete">
+           <label class="md-title">{{timestamp}}</label>
+          <!-- <div class="md-autocomplete">
             <md-autocomplete
               class="search"
               v-model="selectedEmployee"
               :md-options="employees"
             >
-              <label>Search...</label>
+              <label>{{timestamp}}</label>
             </md-autocomplete>
-          </div>
+          </div> -->
           <md-list>
             <md-list-item href="#/">
               <i class="material-icons">dashboard</i>
@@ -76,10 +77,37 @@
               </a>
             </li>
 
-            <md-list-item href="#/user">
+              <li class="md-list-item">
+              <a class="md-list-item-router md-list-item-container md-button-clean dropdown">
+                <div class="md-list-item-content">
+                  <drop-down>
+                    <md-button
+                      slot="title"
+                      class="md-button md-just-icon md-simple"
+                      data-toggle="dropdown"
+                    >
+                      <md-icon>person</md-icon>
+                      
+                      <p class="hidden-lg hidden-md">User</p>
+                    </md-button>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                      <li><h5>{{this.$store.state.loggedInUser}}</h5></li>
+                      <li><a href="#/main">Logout</a></li>
+                      
+                      <!-- <li><a href="#">You have 5 new tasks</a></li>
+                      <li><a href="#">You're now friend with Andrew</a></li>
+                      <li><a href="#">Another Notification</a></li>
+                      <li><a href="#">Another One</a></li> -->
+                    </ul>
+                  </drop-down>
+                </div>
+              </a>
+            </li>
+
+            <!-- <md-list-item >
               <i class="material-icons">person</i>
               <p class="hidden-lg hidden-md">Profile</p>
-            </md-list-item>
+            </md-list-item> -->
           </md-list>
         </div>
       </div>
@@ -91,6 +119,7 @@
 export default {
   data() {
     return {
+      timestamp:"",
       selectedEmployee: null,
       employees: [
         "Jim Halpert",
@@ -104,10 +133,20 @@ export default {
       ]
     };
   },
+  created() {
+                setInterval(this.getNow, 1000);
+            },
   methods: {
     toggleSidebar() {
       this.$sidebar.displaySidebar(!this.$sidebar.showSidebar);
-    }
+    },
+    getNow: function() {
+                    const today = new Date();
+                    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    const dateTime = date +' '+ time;
+                    this.timestamp = dateTime;
+                }
   }
 };
 </script>
